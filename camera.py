2e46ecfa62cac0d96761ab0f18cd52b271fbc5bc
无劼 <48663350+willwoodson@users.noreply.github.com>
2019-09-07 17:30:49 +0800

    first commit

---
+++ camera.py
@@ -0,0 +1,26 @@
+import cv2
+import time
+
+
+webcam = cv2.VideoCapture(0)
+
+# 定义编解码器并创建 VideoWriter 对象
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('data/example.avi',fourcc, 20.0, (640,480))
+
+while True:
+    # 我们从网络摄像头中得到一个新的画面
+    _, frame = webcam.read()
+
+    if cv2.waitKey(0) == ord('s'):
+        name = "eyes/" + str(time.time())+ ".jpg"
+        cv2.imwrite(name, frame)
+
+    # 保存当前帧
+    # out.write(frame)
+    cv2.imshow("Demo", frame)
+
+    if cv2.waitKey(0) == 27:
+        break
+
+cv2.destroyAllWindows()
